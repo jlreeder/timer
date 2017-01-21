f7baafbe0ff2cd4c657fadf107aac4d3c0e25993
Jason Reeder <info@jasonreeder.com>
2017-01-20 16:20:09 -0800

    Add

--- timer.rb
+++ timer.rb
@@ -1,7 +1,26 @@
-def notify(message, title)
-  system("terminal-notifier -message \"#{message}\" -title \"#{title}\"")
+def set_timer(minutes)
+  puts "Timer set for #{minutes} minutes"
+  timer(minutes)
+  notify(minutes_message(minutes))
+end
+
+def timer(minutes)
+  i = 0
+  until i == minutes
+    sleep(50)
+    i += 1
+    print "#{i} "
+  end
+end
+
+def notify(body, title)
+  system("terminal-notifier -message \"#{body}\" -title \"#{title}\"")
+end
+
+def minutes_message(minutes)
+  ["#{minutes} complete", 'Timer Complete']
 end
 
 if __FILE__ == $PROGRAM_NAME
-  notify("test message", "message")
+  set_timer(10)
 end
