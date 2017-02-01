7d2597e97ca6a1283c6cddb4af32521853fb3a1f
Jason Reeder <info@jasonreeder.com>
2017-01-31 20:58:52 -0800

    Add ability to specify task

--- timer.rb
+++ timer.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-def set_timer(minutes)
-  puts "Timer set for #{minutes} minutes"
+def set_timer(minutes, task)
+  puts "Timer set for #{minutes} minutes."
   timer(minutes)
-  body, title = minutes_message(minutes)
-  notify(body, title)
+  message = minutes_message(minutes)
+  notify(message, task)
   puts "\nComplete"
 end
 
@@ -17,14 +17,22 @@ def timer(minutes)
   end
 end
 
-def notify(body, title)
-  system("terminal-notifier -message \"#{body}\" -title \"#{title}\"")
+def notify(message, task)
+  system("terminal-notifier -message \"#{message}\" -title \"#{task}\"")
 end
 
 def minutes_message(minutes)
-  ["#{minutes} minutes have passed", 'Timer Complete']
+  "#{minutes} minutes have passed"
+end
+
+def process_time(raw_time)
+  raw_time.to_i
+end
+
+def process_task(raw_task)
+  raw_task.capitalize || 'Timer complete'
 end
 
 if __FILE__ == $PROGRAM_NAME
-  set_timer(ARGV[0].to_i)
+  set_timer(process_time(ARGV[0]), process_task(ARGV[1]))
 end
